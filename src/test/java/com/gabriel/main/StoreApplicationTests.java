5e73acc890bd641ccb486968275e13a6aa2cd697
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-08-15 17:36:34 -0300

    Projeto criado

---
+++ src/test/java/com/gabriel/main/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
