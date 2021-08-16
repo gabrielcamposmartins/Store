f5754e1b967443379aa59dcb0c3f926e8b417073
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-08-16 09:50:49 -0300

    Criação, mapeamento do produto e instanciação para teste

--- src/main/java/com/gabriel/main/StoreApplication.java
+++ src/main/java/com/gabriel/main/StoreApplication.java
@@ -1,13 +1,49 @@
 package com.gabriel.main;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gabriel.main.domain.Categoria;
+import com.gabriel.main.domain.Produto;
+import com.gabriel.main.repositories.CategoriaRepository;
+import com.gabriel.main.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class StoreApplication {
+public class StoreApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 50.00);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
+		categoriaRepository.saveAll(Arrays.asList(c1,c2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+	}
+	
+	
 
 }
