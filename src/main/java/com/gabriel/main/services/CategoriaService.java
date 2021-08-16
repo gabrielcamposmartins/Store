fb8174edcbfa4aa78f730ec123478c0aeb642899
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-08-16 19:26:50 -0300

    Tratando exceções

--- src/main/java/com/gabriel/main/services/CategoriaService.java
+++ src/main/java/com/gabriel/main/services/CategoriaService.java
@@ -1,10 +1,13 @@
 package com.gabriel.main.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.main.domain.Categoria;
 import com.gabriel.main.repositories.CategoriaRepository;
+import com.gabriel.main.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -13,7 +16,9 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria Buscar(Integer id){
-		Categoria categoria = repo.findById(id).get();
-		return categoria;
+		Optional<Categoria> categoria = repo.findById(id);
+
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
