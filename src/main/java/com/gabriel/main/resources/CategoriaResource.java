262c47d967a1cddd0484f115d28fa0981b6fd7bf
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-08-24 18:39:39 -0300

    Paginação com parâmetros na categoria

--- src/main/java/com/gabriel/main/resources/CategoriaResource.java
+++ src/main/java/com/gabriel/main/resources/CategoriaResource.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -63,5 +65,16 @@ public class CategoriaResource {
 		}
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/page",method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDTO = list.map(categoria -> new CategoriaDTO(categoria));
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
