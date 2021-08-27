9d6ebc873dba0f8576e5e802d17199d3bce7947f
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-08-27 17:50:19 -0300

    GET, PUT e DELETE de cliente

--- src/main/java/com/gabriel/main/resources/ClienteResource.java
+++ src/main/java/com/gabriel/main/resources/ClienteResource.java
@@ -1,14 +1,27 @@
 package com.gabriel.main.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.main.domain.Cliente;
+import com.gabriel.main.domain.Cliente;
+
+import com.gabriel.main.dto.ClienteDTO;
 import com.gabriel.main.services.ClienteService;
+import com.gabriel.main.services.exceptions.DataIntegrityException;
 
 @RestController
 @RequestMapping(value="/clientes")
@@ -19,7 +32,46 @@ public class ClienteResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-		Cliente cliente = this.service.Buscar(id);
+		Cliente cliente = this.service.find(id);
 		return ResponseEntity.ok().body(cliente);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO clienteDto){
+		Cliente cliente = service.fromDTO(clienteDto);
+		cliente = service.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody ClienteDTO clienteDto, @PathVariable Integer id){
+		Cliente cliente = service.fromDTO(clienteDto);
+		cliente.setId(id);
+		cliente = service.update(cliente);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Cliente> delete(@PathVariable Integer id) {
+		try {
+			service.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Impossível excluir cliente com produtos!");
+		}
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/page",method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(cliente -> new ClienteDTO(cliente));
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/gabriel/main/services/ClienteService.java
+++ src/main/java/com/gabriel/main/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.gabriel.main.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.main.domain.Cliente;
+import com.gabriel.main.dto.ClienteDTO;
 import com.gabriel.main.repositories.ClienteRepository;
+import com.gabriel.main.services.exceptions.DataIntegrityException;
 import com.gabriel.main.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,10 +22,49 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente Buscar(Integer id){
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Cliente find(Integer id){
 		Optional<Cliente> cliente = repo.findById(id);
 
 		return cliente.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return repo.save(cliente);
+	}
+	
+	public Cliente update(Cliente cliente) {
+		Cliente novo = find(cliente.getId());
+		updateData(novo, cliente);
+		return repo.save(novo);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir, cliente possui entidades relacionadas");
+		}
+		repo.deleteById(id);
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDto) {
+		return new Cliente(clienteDto.getId(), clienteDto.getNome(), clienteDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente novo, Cliente cliente) {
+		novo.setNome(cliente.getNome());
+		novo.setEmail(cliente.getEmail());
 	}
 }
